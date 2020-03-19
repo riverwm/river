7b735f690c46f8665627bd9ba5c9019462404aaf
Isaac Freund <ifreund@ifreund.xyz>
2020-03-19 21:35:01 +0100

    cleanup

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("dwc", "src/main.zig");
+    const exe = b.addExecutable("zag", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.linkLibC();
