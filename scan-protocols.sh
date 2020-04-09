86c486bf2dd589f67160b56aef4d4aae66005f77
Isaac Freund <ifreund@ifreund.xyz>
2020-04-09 13:18:19 +0200

    Import layer shell protocol

--- scan-protocols.sh
+++ scan-protocols.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 wayland-scanner server-header protocol/xdg-shell.xml protocol/xdg-shell-protocol.h
 wayland-scanner private-code protocol/xdg-shell.xml protocol/xdg-shell-protocol.c
+
+wayland-scanner server-header protocol/wlr-layer-shell-unstable-v1.xml protocol/wlr-layer-shell-unstable-v1-protocol.h
+wayland-scanner private-code protocol/wlr-layer-shell-unstable-v1.xml protocol/wlr-layer-shell-unstable-v1-protocol.c
