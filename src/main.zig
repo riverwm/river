8a9c2182eabadf6e50532b2b9a1044859587af82
Isaac Freund <ifreund@ifreund.xyz>
2020-05-02 19:21:10 +0200

    Relicense to GPL-3.0-or-later

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,25 @@
-const std = @import("std");
-const c = @import("c.zig");
-const Log = @import("log.zig").Log;
+// This file is part of river, a dynamic tiling wayland compositor.
+//
+// Copyright 2020 Isaac Freund
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+const std = @import("std");
+
+const c = @import("c.zig");
+
+const Log = @import("log.zig").Log;
 const Server = @import("server.zig");
 
 pub fn main() !void {
--- src/xdg_popup.zig
+++ src/xdg_popup.zig
@@ -1,7 +1,25 @@
+// This file is part of river, a dynamic tiling wayland compositor.
+//
+// Copyright 2020 Isaac Freund
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 const Self = @This();
 
-const c = @import("c.zig");
 const std = @import("std");
+
+const c = @import("c.zig");
 
 const XdgToplevel = @import("xdg_toplevel.zig");
 
