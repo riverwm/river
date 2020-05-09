26cced20d9ee31390a7bae5210eb8ec3f7e69b98
Isaac Freund <ifreund@ifreund.xyz>
2020-05-09 19:40:39 +0200

    Give xwayland views the right x/y coords
    That wasn't really too hard now, was it?

--- src/XwaylandView.zig
+++ src/XwaylandView.zig
@@ -57,12 +57,12 @@ pub fn init(self: *Self, view: *View, wlr_xwayland_surface: *c.wlr_xwayland_surf
     c.wl_signal_add(&self.wlr_xwayland_surface.events.unmap, &self.listen_unmap);
 }
 
-/// TODO: real output layout coords
+/// Tell the client to take a new size
 pub fn configure(self: Self, pending_box: Box) void {
     c.wlr_xwayland_surface_configure(
         self.wlr_xwayland_surface,
-        0,
-        0,
+        @intCast(i16, pending_box.x),
+        @intCast(i16, pending_box.y),
         @intCast(u16, pending_box.width),
         @intCast(u16, pending_box.height),
     );
