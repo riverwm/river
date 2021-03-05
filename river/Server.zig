25cd99acfe771490ddb7b3161e53922e66e1c285
Marten Ringwelski <git@maringuu.de>
2021-03-05 12:20:40 +0100

    Implement idle-inhibit-unstable-v1

--- river/Server.zig
+++ river/Server.zig
@@ -125,6 +125,7 @@ pub fn init(self: *Self) !void {
     _ = try wlr.DataControlManagerV1.create(self.wl_server);
     _ = try wlr.ExportDmabufManagerV1.create(self.wl_server);
     _ = try wlr.GammaControlManagerV1.create(self.wl_server);
+    _ = try wlr.IdleInhibitManagerV1.create(self.wl_server);
     _ = try wlr.ScreencopyManagerV1.create(self.wl_server);
     _ = try wlr.Viewporter.create(self.wl_server);
 }
