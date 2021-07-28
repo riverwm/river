6e8c4dc25db4cf1ed407b169c1df6da7e46aedd1
HeavRain266 <ignacy423@gmail.com>
2021-07-28 21:54:33 +0200

    ci: build wlroots through install_deps.sh

--- .github/workflows/install_deps.sh
+++ .github/workflows/install_deps.sh
@@ -8,3 +8,9 @@ xbps-install -uy MesaLib-devel libseat-devel eudev-libudev-devel libdrm-devel \
   xcb-util-cursor-devel xcb-util-devel xcb-util-image-devel xcb-util-keysyms-devel \
   xcb-util-xrm-devel xorg-server-xwayland pkg-config meson git gcc \
   zig pkgconf scdoc
+
+git clone https://github.com/swaywm/wlroots.git
+cd wlroots
+git checkout 0.14.0
+meson build --auto-features=enabled -Dexamples=false -Dwerror=false -Db_ndebug=false
+ninja -C build install
