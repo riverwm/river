riverwm/river

commit 56c1017c24bfbded9b0c40e12655652828079f16

InputDevice: fix cleanup
These things are only initialized for non-virtual input devices.
Therefore, only clean them up for non-virtual input devices.

river/InputDevice.zig
@@ -126,11 +126,21 @@ pub fn deinit(device: *InputDevice) void {
     assert(device.objects.empty());
     device.remove.link.remove();
 
-    if (device.wlr_device.getLibinputDevice() != null) {
-        device.libinput.deinit();
-    }
-    if (device.wlr_device.type == .keyboard) {
-        device.xkb_keyboard.deinit();
+    if (!device.virtual) {
+        {
+            var it = device.objects.iterator(.forward);
+            while (it.next()) |object| {
+                object.getLink().remove();
+                object.sendRemoved();
+                object.setHandler(?*anyopaque, handleRequestInert, null, null);
+            }
+        }
+        if (device.wlr_device.getLibinputDevice() != null) {
+            device.libinput.deinit();
+        }
+        if (device.wlr_device.type == .keyboard) {
+            device.xkb_keyboard.deinit();
+        }
     }
 
     device.link.remove();
@@ -157,15 +167,6 @@ fn handleRemove(listener: *wl.Listener(*wlr.InputDevice), _: *wlr.InputDevice) v
         @tagName(device.wlr_device.type),
         device.wlr_device.name orelse "unknown",
     });
-
-    {
-        var it = device.objects.iterator(.forward);
-        while (it.next()) |object| {
-            object.getLink().remove();
-            object.sendRemoved();
-            object.setHandler(?*anyopaque, handleRequestInert, null, null);
-        }
-    }
 
     switch (device.wlr_device.type) {
         .keyboard => {

commit 6b9f40ca72dc4be6ef5866fbb78a3464a1941071

InputDevice: fix assertion failure
Fixes: 56c1017c24bfbded9b0c40e12655652828079f16

river/InputDevice.zig
@@ -123,9 +123,6 @@ pub fn createObject(device: *InputDevice, im_v1: *river.InputManagerV1) void {
 }
 
 pub fn deinit(device: *InputDevice) void {
-    assert(device.objects.empty());
-    device.remove.link.remove();
-
     if (!device.virtual) {
         {
             var it = device.objects.iterator(.forward);
@@ -143,6 +140,7 @@ pub fn deinit(device: *InputDevice) void {
         }
     }
 
+    device.remove.link.remove();
     device.link.remove();
     device.seat.updateCapabilities();
 