ed4efb58254983a054af42705d882ad019d636ca
Isaac Freund <mail@isaacfreund.com>
2026-05-12 09:45:22 +0200

    build: don't override use_llvm/lld defaults
    Only set the use_llvm/use_lld options if -Dllvm is actually passed to
    zig build.
    
    Fixes: 7cb05dd791b983094487d7cbbea80d5b653512c9
    (cherry picked from commit 8a1afd94ca477045f39bf91ac9ec31da9e9f932a)

--- build.zig
+++ build.zig
@@ -19,7 +19,7 @@ pub fn build(b: *Build) !void {
 
     const strip = b.option(bool, "strip", "Omit debug information") orelse false;
     const pie = b.option(bool, "pie", "Build a Position Independent Executable") orelse false;
-    const use_llvm = b.option(bool, "llvm", "Force use of Zig's LLVM backend and the lld linker") orelse false;
+    const use_llvm = b.option(bool, "llvm", "Force use of Zig's LLVM backend and the lld linker");
 
     const omit_frame_pointer = switch (optimize) {
         .Debug, .ReleaseSafe => false,
