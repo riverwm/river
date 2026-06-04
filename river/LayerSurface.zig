04c5c975c22c8422a0f0d94344c961b3b1d0acd2
Isaac Freund <mail@isaacfreund.com>
2026-06-04 15:58:20 +0200

    LayerSurface: fix possible assertion failure
    This can be hit during a race during output destruction.
    
    Fixes: 65e1199d8403e74bc5705d12c8c530265d1afe21
    (cherry picked from commit 76b9f619c74a1ddebfad3c798b189b5458b06009)

--- river/LayerSurface.zig
+++ river/LayerSurface.zig
@@ -151,7 +151,8 @@ fn handleCommit(listener: *wl.Listener(*wlr.Surface), _: *wlr.Surface) void {
     const layer_surface: *LayerSurface = @fieldParentPtr("commit", listener);
     const wlr_layer_surface = layer_surface.wlr_layer_surface;
 
-    assert(wlr_layer_surface.output != null);
+    // Output is being destroyed, see LayerShell.destroySurfaces()
+    if (wlr_layer_surface.output == null) return;
 
     // If the layer was changed, move the LayerSurface to the proper tree.
     if (wlr_layer_surface.current.committed.layer) {
