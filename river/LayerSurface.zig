riverwm/river

commit 65e1199d8403e74bc5705d12c8c530265d1afe21

Output: fix potential assertion failure on destroy
Currently the following wlroots assertion can fail:
assert(wl_list_empty(&output->events.bind.listener_list))

The reproduction I have isolated requires the following conditions:

1. DRM backend
2. One single output
3. A layer shell surface with an opaque region set
4. One or more other surfaces that are fully occluded by the layer shell
surface from the very instant they are mapped.
5. Switch to a different tty to reproduce the crash.

However, I strongly suspect there are other conditions that can also
trigger this assertion failure.

This happens due to the wlr_layer_surface_destroy() call in the
Output destroy listener, which modifies the scene graph and can
cause wlr_surface_send_enter() to be called for the output being
destroyed under certain circumstances (e.g. the reproducer above).

Since the wlr_scene code is highly complicated, I believe the only safe
thing to do is not call any wlr_scene functions in the Output destroy
listener.

Closes: https://codeberg.org/river/river/issues/1473

river/LayerSurface.zig
@@ -136,11 +136,15 @@ fn handleUnmap(listener: *wl.Listener(void)) void {
         }
     }
 
-    // Beware: it is possible for arrange() to destroy this LayerSurface!
-    const output: *Output = @ptrCast(@alignCast(layer_surface.wlr_layer_surface.output.?.data));
-    output.layer_shell.arrange();
-    server.layer_shell.checkExclusiveFocus();
-    server.wm.dirtyWindowing();
+    // wlr_layer_surface.output is null when the layer surface is unmapped due
+    // to the output being destroyed.
+    if (layer_surface.wlr_layer_surface.output) |wlr_output| {
+        // Beware: it is possible for arrange() to destroy this LayerSurface!
+        const output: *Output = @ptrCast(@alignCast(wlr_output.data));
+        output.layer_shell.arrange();
+        server.layer_shell.checkExclusiveFocus();
+        server.wm.dirtyWindowing();
+    }
 }
 
 fn handleCommit(listener: *wl.Listener(*wlr.Surface), _: *wlr.Surface) void {

river/LayerShell.zig
@@ -35,6 +35,8 @@ surfaces: SlotMap(*LayerSurface) = .empty,
 
 new_surface: wl.Listener(*wlr.LayerSurfaceV1) = .init(handleNewSurface),
 
+destroy_surfaces: ?*wl.EventSource = null,
+
 pub fn init(layer_shell: *LayerShell) !void {
     layer_shell.* = .{
         .global = try wl.Global.create(server.wl_server, river.LayerShellV1, 1, *LayerShell, layer_shell, bind),
@@ -190,4 +192,37 @@ pub fn checkExclusiveFocus(_: *LayerShell) void {
             seat.layer_shell.scheduled.focus = .none;
         }
     }
+}
+
+/// This exists to workaround a terribly subtle wlroots bug that can happen if
+/// wlr_layer_surface.destroy() is called directly in Output.handleDestroy().
+/// I intend to fix this bug upstream in wlroots if possible, but the root cause
+/// is the complexity of wlr_scene and I want river to use wlr_scene in the
+/// simplest way possible to avoid regressions in the future. Therefore this
+/// workaround should be kept even if the specific bug is fixed upstream.
+pub fn destroySurfaces(layer_shell: *LayerShell, output: *Output) void {
+    var it = layer_shell.surfaces.iterator();
+    while (it.next()) |surface| {
+        if (surface.wlr_layer_surface.output == output.wlr_output) {
+            surface.wlr_layer_surface.output = null;
+        }
+    }
+
+    if (layer_shell.destroy_surfaces == null) {
+        const event_loop = server.wl_server.getEventLoop();
+        layer_shell.destroy_surfaces = event_loop.addIdle(*LayerShell, destroySurfacesIdle, layer_shell) catch {
+            log.err("out of memory", .{});
+            return;
+        };
+    }
+}
+
+fn destroySurfacesIdle(layer_shell: *LayerShell) void {
+    var it = layer_shell.surfaces.iterator();
+    while (it.next()) |surface| {
+        if (surface.wlr_layer_surface.output == null) {
+            surface.wlr_layer_surface.destroy();
+        }
+    }
+    layer_shell.destroy_surfaces = null;
 }

river/Output.zig
@@ -249,14 +249,8 @@ fn handleDestroy(listener: *wl.Listener(*wlr.Output), wlr_output: *wlr.Output) v
 
     log.debug("wlr_output '{s}' destroyed", .{wlr_output.name});
 
-    {
-        var it = server.layer_shell.surfaces.iterator();
-        while (it.next()) |surface| {
-            if (surface.wlr_layer_surface.output == wlr_output) {
-                surface.wlr_layer_surface.destroy();
-            }
-        }
-    }
+    server.layer_shell.destroySurfaces(output);
+
     {
         var it = server.input_manager.devices.iterator(.forward);
         while (it.next()) |device| {

commit 76b9f619c74a1ddebfad3c798b189b5458b06009

LayerSurface: fix possible assertion failure
This can be hit during a race during output destruction.

Fixes: 65e1199d8403e74bc5705d12c8c530265d1afe21

river/LayerSurface.zig
@@ -151,7 +151,8 @@ fn handleCommit(listener: *wl.Listener(*wlr.Surface), _: *wlr.Surface) void {
     const layer_surface: *LayerSurface = @fieldParentPtr("commit", listener);
     const wlr_layer_surface = layer_surface.wlr_layer_surface;
 
-    assert(wlr_layer_surface.output != null);
+    // Output is being destroyed, see LayerShell.destroySurfaces()
+    if (wlr_layer_surface.output == null) return;
 
     // If the layer was changed, move the LayerSurface to the proper tree.
     if (wlr_layer_surface.current.committed.layer) {